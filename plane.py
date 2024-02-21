921db5ee86afb04d0e78ca840819ddd5d56c528c
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-21 17:10:19 +0300

    010

--- plane.py
+++ plane.py
@@ -11,7 +11,7 @@ def template_html_plane(hashMap,_files=None,_data=None):
         htmltxt = file1_.read()
 
     planename = suClass.get_stored_file("plane")
-    with open(planename, encoding='utf-8') as file2_:
+    with open(planename, encoding='base64') as file2_:
         planetxt = file2_.read()
     #htmltxt.replace("###",planetxt)
 
