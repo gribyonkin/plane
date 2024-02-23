fa4ba72106ae61d1c615b8e4a5ee0041255d87a4
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-23 09:48:21 +0300

    015

--- plane.py
+++ plane.py
@@ -14,8 +14,8 @@ def template_html_plane(hashMap,_files=None,_data=None):
 
     planename = suClass.get_stored_file("plane")
     hashMap.put("toast", planename)
-    with open(planename,"rb") as file2_:
-        encoded_string = base64.b64encode(file2_.read())
+    #with open(planename,"rb") as file2_:
+    #    encoded_string = base64.b64encode(file2_.read())
     #    planetxt = file2_.read()
     #htmltxt.replace("###",planetxt)
 
