0c4791a2027b8c2920d4ecb16cd3a38b2a20a4a2
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-23 19:36:53 +0300

    455

--- plane.py
+++ plane.py
@@ -8,8 +8,8 @@ def starter(hashMap,_files=None,_data=None):
     planename = suClass.get_stored_file("all")
     #hashMap.put("toast", planename)
     #hashMap.put("pic_str", planename)
-    with open(filename, "rb") as image_file:
-      encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    with open(planename, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
     #with open(planename) as file2_:
     #    encoded_string = base64.b64encode(file2_.read())
