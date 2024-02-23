6f5a065186eff907e530bfeca1d564df72fadf1a
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-23 20:44:12 +0300

    kjg

--- plane.py
+++ plane.py
@@ -28,21 +28,21 @@ def template_html_plane(hashMap,_files=None,_data=None):
     import base64
     from io import BytesIO
    
-    filename = suClass.get_stored_file("template1")
+    filename = suClass.get_stored_file("template2")
     #hashMap.put("toast", filename)
     with open(filename) as file1_:
         htmltxt = file1_.read()
 
-    planename = suClass.get_stored_file("plane")
+    #planename = suClass.get_stored_file("plane")
     #hashMap.put("toast", planename)
-    with open(planename,"rb") as file2_:
-        encoded_string = base64.b64encode(file2_.read()).decode('utf-8')
+    #with open(planename,"rb") as file2_:
+    #    encoded_string = base64.b64encode(file2_.read()).decode('utf-8')
         #encoded_string = file2_.read()
     #    planetxt = file2_.read()
-    htmltxt.replace("###",encoded_string)
+    #htmltxt.replace("###",encoded_string)
     #htmltxt.replace("###",planename)
     #hashMap.put("toast", encoded_string)
-    htmltxt.replace("&nbsp"," ")
+    #htmltxt.replace("&nbsp"," ")
     hashMap.put("html_plane_all", htmltxt)
 
     return hashMap
