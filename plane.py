e1abc70bc9805c46855a4541948255298be10af8
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-24 11:24:32 +0300

    c[ptr

--- plane.py
+++ plane.py
@@ -23,6 +23,7 @@ def starter(hashMap,_files=None,_data=None):
     #htmltxt.replace("###",encoded_string)
     #htmltxt.replace("###",planename)
     #hashMap.put("toast", encoded_string)
+    encoded_string.replace("&nbsp"," ")    
     hashMap.put("pic_str", encoded_string)
     #htmltxt.replace("&nbsp"," ")
     #hashMap.put("html_plane_all", htmltxt)
