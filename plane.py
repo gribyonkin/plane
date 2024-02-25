b9376da06404bf3df8885d9392263b320e906ef3
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-25 19:22:09 +0300

    kfg

--- plane.py
+++ plane.py
@@ -63,7 +63,8 @@ def starter4(hashMap,_files=None,_data=None):
     with open(filename) as file1_:
         htmltxt = file1_.read()
 
-    #planename = suClass.get_stored_file("mainplane")
+    planename = suClass.get_stored_file("pl")
+    hashMap.put("pl_file", planename)
     #hashMap.put("toast", planename)
     #with open(planename,"rb") as file2_:
     #    encoded_string = base64.b64encode(file2_.read()).decode('utf-8')
