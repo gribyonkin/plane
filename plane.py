f70871097210d3435f0b275b864b61ad383f2136
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-26 05:08:48 +0300

    3rs

--- plane.py
+++ plane.py
@@ -68,7 +68,7 @@ def starter4(hashMap,_files=None,_data=None):
     #hashMap.put("toast", planename)
     with open(planename,"rb") as file2_:
         encoded_string = base64.b64encode(file2_.read()).decode('utf-8')
-    encoded_string.replace("&nbsp"," ")
+    encoded_string.replace("&nbsp","")
     hashMap.put("toast", encoded_string)
   
     htmltxt.replace("###",encoded_string)
